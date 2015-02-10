436420c81a90ead6d3001c7ebc681dd212e8aa4f
Michael Church <michael@fishinglounge.com>
2015-02-10 17:13:30 -0500

    Progress development

--- wp-content/themes/socrata-bootstrap/full-width.php
+++ wp-content/themes/socrata-bootstrap/full-width.php
@@ -4,15 +4,14 @@ Template Name: Full Width
 */
 get_header(); ?>
 <div id="page">
-	<div class="container content">
-		<div class="row">
-			<div class="col-xs-12">
-		  		<?php if(have_posts()): while(have_posts()): the_post();?>
-	          	<?php the_content()?>
-		        <?php endwhile; ?>
-		        <?php endif;?>
-			</div>
-		</div>
+	<div class="content">
+
+  		<?php if(have_posts()): while(have_posts()): the_post();?>
+      	<?php the_content()?>
+        <?php endwhile; ?>
+        <?php endif;?>
+
 	</div>
 </div>
+<?php get_template_part( 'call-to-action' ); ?>
 <?php get_footer(); ?>
