b2152f5ae3eb37f30c35a724c704d4c727a1c8ff
Michael Church <michael@fishinglounge.com>
2015-03-24 15:46:43 -0400

    progress dev

--- wp-content/themes/socrata-bootstrap/shortcodes/timeline.php
+++ wp-content/themes/socrata-bootstrap/shortcodes/timeline.php
@@ -17,9 +17,8 @@ function socrata_timeline() {
       <div class="socrata-timeline-dot">
       </div> <!-- socrata-timeline-dot -->
       <div class="socrata-timeline-content">
-        <h4>CEO Kevin Merritt Founds Socrata</h4>
-        <p>This cloud platform enables public sector organizations to easily manage and share their data.</p>
-        <p><img src="http://placehold.it/100x100"></p>
+        <h4>2007: Kevin Merritt Founds Blist</h4>
+        <p>blist (short for web-list) is designed as a database for mainstream users.</p>
       </div> <!-- socrata-timeline-content -->
     </div> <!-- socrata-timeline-block -->
 
@@ -27,8 +26,8 @@ function socrata_timeline() {
       <div class="socrata-timeline-dot">
       </div> <!-- socrata-timeline-dot -->
       <div class="socrata-timeline-content">
-        <h4>Dapibus Vulputate Consectetur</h4>
-        <p>Maecenas faucibus mollis interdum. Donec ullamcorper nulla non metus auctor fringilla.</p>
+        <h4> March 2007: blist Hires First Employee</h4>
+        <p>By the end of 2007, blist has six employees on staff, including the founder.</p>
       </div> <!-- socrata-timeline-content -->
     </div> <!-- socrata-timeline-block -->
 
@@ -36,8 +35,8 @@ function socrata_timeline() {
       <div class="socrata-timeline-dot">
       </div> <!-- socrata-timeline-dot -->
       <div class="socrata-timeline-content">
-        <h4>Dapibus Vulputate Consectetur</h4>
-        <p>Maecenas faucibus mollis interdum. Donec ullamcorper nulla non metus auctor fringilla.</p>
+        <h4>February 2008: blist raises $6.5 Million Series A</h4>
+        <p>The funds are invested by Frazier Venture Partners and Morgenthaler Ventures. "<i>It&rsquo;s extremely satisfying to have two prestigious investment partners believe in blist&rsquo;s mission of building the world&rsquo;s easiest database and who see the enormous opportunity to breathe new life and innovation into the database market.”</i> -Kevin Merritt</p>
       </div> <!-- socrata-timeline-content -->
     </div> <!-- socrata-timeline-block -->
 
@@ -45,8 +44,8 @@ function socrata_timeline() {
       <div class="socrata-timeline-dot">
       </div> <!-- socrata-timeline-dot -->
       <div class="socrata-timeline-content">
-        <h4>Dapibus Vulputate Consectetur</h4>
-        <p>Maecenas faucibus mollis interdum. Donec ullamcorper nulla non metus auctor fringilla.</p>
+        <h4>February 2008: Ken Gullicksen and Scott Darling Join Blist Board</h4>
+        <p>As part of their investment in the Series A fundraising, Ken Gullicksen and Scott Darling become blist’s first board members.</p>
       </div> <!-- socrata-timeline-content -->
     </div> <!-- socrata-timeline-block -->
 
@@ -54,8 +53,8 @@ function socrata_timeline() {
       <div class="socrata-timeline-dot">
       </div> <!-- socrata-timeline-dot -->
       <div class="socrata-timeline-content">
-        <h4>Dapibus Vulputate Consectetur</h4>
-        <p>Maecenas faucibus mollis interdum. Donec ullamcorper nulla non metus auctor fringilla.</p>
+        <h4>December 2008: blist Goes Presidential</h4>
+        <p>In late 2008, Kevin Merritt has a conversation with The Obama Campaign Tech Team, starting a dialogue that culminates in whitehouse.gov becoming one of our earliest customers.</p>
       </div> <!-- socrata-timeline-content -->
     </div> <!-- socrata-timeline-block -->
 
@@ -63,10 +62,97 @@ function socrata_timeline() {
       <div class="socrata-timeline-dot">
       </div> <!-- socrata-timeline-dot -->
       <div class="socrata-timeline-content">
-        <h4>Dapibus Vulputate Consectetur</h4>
-        <p>Maecenas faucibus mollis interdum. Donec ullamcorper nulla non metus auctor fringilla.</p>
+        <h4>May 2009: Robert Runge Joins Board</h4>
+        <p>“<i>Having been closely involved in blist’s strategy to focus on the public sector and to build a best-of-breed platform to sell directly to governments to encourage and support the emerging open data market, I was excited about the mission and the market.</i>” - Robert Runge</p>
       </div> <!-- socrata-timeline-content -->
     </div> <!-- socrata-timeline-block -->
+
+    <div class="socrata-timeline-block">
+      <div class="socrata-timeline-dot">
+      </div> <!-- socrata-timeline-dot -->
+      <div class="socrata-timeline-content">
+        <h4>May 2009: blist Becomes Socrata</h4>
+        <p>In what will be an historical pivot for the company, blist changes focus from databases to open data. To reflect this new direction, blist becomes Socrata, a name suggested by Board member Robert Runge.</p>
+      </div> <!-- socrata-timeline-content -->
+    </div> <!-- socrata-timeline-block -->
+
+    <div class="socrata-timeline-block">
+      <div class="socrata-timeline-dot">
+      </div> <!-- socrata-timeline-dot -->
+      <div class="socrata-timeline-content">
+        <h4>August 2010: Oregon Becomes First State Customer</h4>
+        <p>Socrata is proud to bring the State of Oregon on board. Oregon will build a strong open data program and will go on to win multiple awards for data.oregon.gov.</p>
+      </div> <!-- socrata-timeline-content -->
+    </div> <!-- socrata-timeline-block -->
+
+    <div class="socrata-timeline-block">
+      <div class="socrata-timeline-dot">
+      </div> <!-- socrata-timeline-dot -->
+      <div class="socrata-timeline-content">
+        <h4>July 2011: Kenya Becomes First Customer Outside of the United States</h4>
+      </div> <!-- socrata-timeline-content -->
+    </div> <!-- socrata-timeline-block -->
+
+    <div class="socrata-timeline-block">
+      <div class="socrata-timeline-dot">
+      </div> <!-- socrata-timeline-dot -->
+      <div class="socrata-timeline-content">
+        <h4>October 2012: Socrata Hires 20th Employee</h4>
+      </div> <!-- socrata-timeline-content -->
+    </div> <!-- socrata-timeline-block -->
+
+    <div class="socrata-timeline-block">
+      <div class="socrata-timeline-dot">
+      </div> <!-- socrata-timeline-dot -->
+      <div class="socrata-timeline-content">
+        <h4>June 2013: Socrata Raises $18 Million Series B</h4>
+        <p>OpenView Venture Partners invests in Socrata, leading a successful series B round of fundraising. “<i>We are the open data market leader and we’ve absolutely figured out the product market fit which has translated into strong customer traction and partner interest. We have a deep product backlog, many new customers coming online and new partnerships that need to be operationalized. The funding allows us to make those investments sooner.</i>” - Kevin Merritt</p>
+      </div> <!-- socrata-timeline-content -->
+    </div> <!-- socrata-timeline-block -->
+
+    <div class="socrata-timeline-block">
+      <div class="socrata-timeline-dot">
+      </div> <!-- socrata-timeline-dot -->
+      <div class="socrata-timeline-content">
+        <h4>June 2013: Scott Maxwell Joins Board</h4>
+        <p>“<i>Open data has become a high priority at every level of government, creating enormous opportunities for disruptive innovation in the public sector.</i>” Scott Maxwell</p>
+      </div> <!-- socrata-timeline-content -->
+    </div> <!-- socrata-timeline-block -->
+
+    <div class="socrata-timeline-block">
+      <div class="socrata-timeline-dot">
+      </div> <!-- socrata-timeline-dot -->
+      <div class="socrata-timeline-content">
+        <h4>March 2013: Socrata Launches GovStat</h4>
+        <p>GovStat is the first government performance measurement product, and the first product Socrata launches after the open data portal.</p>
+      </div> <!-- socrata-timeline-content -->
+    </div> <!-- socrata-timeline-block -->
+
+    <div class="socrata-timeline-block">
+      <div class="socrata-timeline-dot">
+      </div> <!-- socrata-timeline-dot -->
+      <div class="socrata-timeline-content">
+        <h4>October 2013: Socrata Hires 50th Employee</h4>
+      </div> <!-- socrata-timeline-content -->
+    </div> <!-- socrata-timeline-block -->
+
+    <div class="socrata-timeline-block">
+      <div class="socrata-timeline-dot">
+      </div> <!-- socrata-timeline-dot -->
+      <div class="socrata-timeline-content">
+        <h4>August 2014: Socrata Hires 100th Employee</h4>
+      </div> <!-- socrata-timeline-content -->
+    </div> <!-- socrata-timeline-block -->
+
+    <div class="socrata-timeline-block">
+      <div class="socrata-timeline-dot">
+      </div> <!-- socrata-timeline-dot -->
+      <div class="socrata-timeline-content">
+        <h4>November 2014: Socrata Raises $30 Million Series C</h4>
+        <p>Sapphire Venture Partners drives the successful series C round of fundraising. “<i>The future is extremely bright, because open data adoption and usage is exploding at all levels of government globally. Open data has become mainstream, and this viral movement is unstoppable and the lifeblood of connected global networks.</i>” - Kevin Merritt</p>
+      </div> <!-- socrata-timeline-content -->
+    </div> <!-- socrata-timeline-block -->
+
 
   </div> <!-- socrata-timeline -->
   <div class="timeline-btn-wrap">
