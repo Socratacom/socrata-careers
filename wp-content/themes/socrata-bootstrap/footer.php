dcb2632a86892cff74d4adc1ac36a41289eff650
Michael Church <michael@fishinglounge.com>
2015-04-15 17:46:25 -0400

    progress dev

--- wp-content/themes/socrata-bootstrap/footer.php
+++ wp-content/themes/socrata-bootstrap/footer.php
@@ -1,20 +1,24 @@
 <footer>
 <div class="container">
   <div class="row">
-    <div class="col-sm-3">
+    <div class="col-sm-4">
       <div class="stacked-logo footer-logo"></div>
       <p class="copyright">Copyright &copy; <?php echo date('Y'); ?><br/>Socrata, Inc.<br/>All rights reserved.</p>
     </div>
-    <div class="col-sm-3 hidden-xs">
+    <div class="col-sm-2 hidden-xs">
       <h3>Sitemap</h3>
       <?php wp_nav_menu( array( 'theme_location' => 'sitemap' )); ?>
     </div>
-    <div class="col-sm-3">
-      <h3>Get In Touch</h3>
+    <div class="col-sm-2">
+      <h3>Seattle</h3>
       <p>83 S. King Street<br>Suite 107<br>Seattle WA, 98104</p>
       <p><strong>T</strong> 206.340.8008</p>
     </div>
-    <div class="col-sm-3">
+    <div class="col-sm-2">
+      <h3>Washington DC</h3>
+      <p>1150 17th Street NW<br>Suite 200<br>Washington, D.C. 20036</p>
+    </div>    
+    <div class="col-sm-2">
       <h3>Join Us</h3>
       <?php wp_nav_menu( array( 'theme_location' => 'social' )); ?>
     </div>
