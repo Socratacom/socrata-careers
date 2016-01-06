c78065fc486f46517be7c073e22d6fab79ddf481
Michael Church <michael@fishinglounge.com>
2016-01-06 10:10:59 -0500

    progress dev

--- wp-content/themes/socrata-bootstrap/header.php
+++ wp-content/themes/socrata-bootstrap/header.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" href="http://careers.socrata.com/favicon.ico">
+  <link rel="shortcut icon" href="https://careers.socrata.com/favicon.ico">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
